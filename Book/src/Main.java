bc1a90b457d08a2c2354271a64ca8c097f7d0ac2
microlong666 <1095289759@qq.com>
2020-08-28 11:51:22 +0800

    update Book

--- Book/src/Main.java
+++ Book/src/Main.java
@@ -10,8 +10,8 @@ public class Main {
     public static class BookSet {
         String[] name = new String[50];   //数组1存储图书名称数组
         int[] state = new int[50];        //数组2存储图书借出状态：0已借出/1可借
-        String[] date=new String[50];     //数组3存储图书借出日期
-        int[] count=new int[50];         //借出次数
+        String[] date = new String[50];     //数组3存储图书借出日期
+        int[] count = new int[50];          //借出次数
         public String[] getName() {
             return name;
         }
